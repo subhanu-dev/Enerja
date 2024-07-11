f3e7a0983661d29886ef823a85967db6c471b96e
subhanu-dev <subhanuchamakaunofficial@gmail.com>
2024-07-11 10:48:25 +0530

    added custom css to input.css

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,22 @@
 // we are making it!
 console.log("yess utto yes!")
+
+/////////////////////////////////navbar scroller
+
+const links = document.querySelectorAll('a');
+
+
+links.forEach(function (link) {
+
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        //scrolling only if the target loc exists.
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{ Monsfont:['Montserrat','sans serif']}
+      fontFamily:{ Monsfont:['Montserrat','sans serif']},
+      boxShadow:{
+        customorange:'0 0px 15px 0px rgb(249, 115, 22)'
+      }
     },
   },
   plugins: [],
